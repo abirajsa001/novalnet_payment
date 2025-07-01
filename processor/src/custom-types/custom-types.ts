f2a8e58d3844ac73dbdfbf86c70a9d17a030dd7e
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-07-01 10:27:25 +0530

    Update custom-types.ts

--- processor/src/custom-types/custom-types.ts
+++ processor/src/custom-types/custom-types.ts
@@ -1,5 +1,3 @@
 export const launchpadPurchaseOrderCustomType = {
   key: 'payment-launchpad-purchase-order',
-  purchaseOrderNumber: 'launchpadPurchaseOrderNumber',
-  invoiceMemo: 'launchpadPurchaseOrderInvoiceMemo',
 };
