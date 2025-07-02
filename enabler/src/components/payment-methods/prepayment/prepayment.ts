18ad29a84a9cc8703068394adcd7d17c446ff617
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-07-02 16:52:37 +0530

    Update prepayment.ts

--- enabler/src/components/payment-methods/prepayment/prepayment.ts
+++ enabler/src/components/payment-methods/prepayment/prepayment.ts
@@ -73,7 +73,7 @@ export class Prepayment extends BaseComponent {
       
       const requestData: PaymentRequestSchemaDTO = {
         paymentMethod: {
-          type: "PREPAYMENT",
+          type: 'PREPAYMENT',
         },
         paymentOutcome: PaymentOutcome.AUTHORIZED,
       };
