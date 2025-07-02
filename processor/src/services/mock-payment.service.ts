ea8d9a96af92fe2c4ecbbcf1873fbedb8ebca331
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-07-02 13:11:48 +0530

    Update mock-payment.service.ts

--- processor/src/services/mock-payment.service.ts
+++ processor/src/services/mock-payment.service.ts
@@ -324,7 +324,7 @@ console.log('status-handler');
 	    input4: 'Payment-Method',
 	    inputval4: String(request.data.paymentMethod.type ?? "Payment-Method not available"),
 	    input5: 'Test Mode',
-	    inputval5: String(getConfig()?.novalnetTestMode ?? '10004'),
+	    inputval5: String(getConfig()?.novalnetPrepaymentTestMode ?? '10004'),
 	  }
 	};
 
