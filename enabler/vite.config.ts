1c88e2d6c6a0926da4fc9f73324dcd1b03411772
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-07-03 18:42:57 +0530

    Update vite.config.ts

--- enabler/vite.config.ts
+++ enabler/vite.config.ts
@@ -1,43 +1,38 @@
-import { resolve } from "path";
-import { defineConfig } from "vite";
-import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
-
-const alias = {
-  '@processor': resolve(__dirname, '../../processor/src'),
-};
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
 
 export default defineConfig({
   resolve: {
-    alias,
+    alias: {
+      '@processor': resolve(__dirname, '../../processor/src'),
+    },
   },
   plugins: [
     cssInjectedByJsPlugin({
-      injectCodeFunction: function injectCodeCustomRunTimeFunction(
-        cssCode: string,
-        options
-      ) {
+      injectCodeFunction(cssCode: string, options) {
         try {
-          if (typeof document != "undefined") {
-            var elementStyle = document.createElement("style");
-            elementStyle.setAttribute("data-ctc-connector-styles", "");
-            for (const attribute in options.attributes) {
-              elementStyle.setAttribute(attribute, options.attributes[attribute]);
+          if (typeof document !== 'undefined') {
+            const style = document.createElement('style');
+            style.setAttribute('data-ctc-connector-styles', '');
+            for (const attr in options.attributes) {
+              style.setAttribute(attr, options.attributes[attr]);
             }
-            elementStyle.appendChild(document.createTextNode(cssCode));
-            document.head.appendChild(elementStyle);
+            style.appendChild(document.createTextNode(cssCode));
+            document.head.appendChild(style);
           }
         } catch (e) {
-          console.error("vite-plugin-css-injected-by-js", e);
+          console.error('vite-plugin-css-injected-by-js', e);
         }
       },
     }),
   ],
   build: {
-    outDir: resolve(__dirname, "public"),
+    outDir: resolve(__dirname, 'public'),
     lib: {
-      entry: resolve(__dirname, "src/main.ts"),
-      name: "Connector",
-      formats: ["es", "umd"],
+      entry: resolve(__dirname, 'src/main.ts'),
+      name: 'Connector',
+      formats: ['es', 'umd'],
       fileName: (format) => `connector-enabler.${format}.js`,
     },
   },
