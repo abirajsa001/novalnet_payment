82c96325e14ebad041c76c0763884c503ab7bf68
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-07-04 16:07:59 +0530

    Update vite.config.ts

--- enabler/vite.config.ts
+++ enabler/vite.config.ts
@@ -7,7 +7,7 @@ import path from 'path';
 export default defineConfig({
     resolve: {
     alias: {
-      '@processor': path.resolve(__dirname, '../processor/src'),
+      '@prepayment': path.resolve(__dirname, '../prepayment/src'),
     },
   },
   plugins: [
