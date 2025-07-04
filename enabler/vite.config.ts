913de3b2e78f2aa536f31ae03783314926b18005
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-07-04 14:46:17 +0530

    Update vite.config.ts

--- enabler/vite.config.ts
+++ enabler/vite.config.ts
@@ -1,6 +1,8 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
+import { defineConfig } from 'vite';
+import path from 'path';
 
 export default defineConfig({
     resolve: {
