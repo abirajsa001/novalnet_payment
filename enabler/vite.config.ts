c1d85e51b73cb703f4376389dfb65e329610f46c
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-07-04 10:41:38 +0530

    Update vite.config.ts

--- enabler/vite.config.ts
+++ enabler/vite.config.ts
@@ -1,38 +1,43 @@
-import { resolve } from 'path';
-import { defineConfig } from 'vite';
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@processor': resolve(__dirname, '../../processor/src'),
-    },
-  },
   plugins: [
     cssInjectedByJsPlugin({
-      injectCodeFunction(cssCode: string, options) {
+      injectCodeFunction: function injectCodeCustomRunTimeFunction(
+        cssCode: string,
+        options
+      ) {
         try {
-          if (typeof document !== 'undefined') {
-            const style = document.createElement('style');
-            style.setAttribute('data-ctc-connector-styles', '');
-            for (const attr in options.attributes) {
-              style.setAttribute(attr, options.attributes[attr]);
+          if (typeof document != "undefined") {
+            var elementStyle = document.createElement("style");
+            // this attribute will allow the client application using this connector to
+            // identify the style tag and remove it if needed for cleanup purposes
+            elementStyle.setAttribute("data-ctc-connector-styles", "");
+            for (const attribute in options.attributes) {
+              elementStyle.setAttribute(
+                attribute,
+                options.attributes[attribute]
+              );
             }
-            style.appendChild(document.createTextNode(cssCode));
-            document.head.appendChild(style);
+            elementStyle.appendChild(document.createTextNode(cssCode));
+            document.head.appendChild(elementStyle);
           }
         } catch (e) {
-          console.error('vite-plugin-css-injected-by-js', e);
+          console.error("vite-plugin-css-injected-by-js", e);
         }
       },
     }),
   ],
   build: {
-    outDir: resolve(__dirname, 'public'),
+    outDir: resolve(__dirname, "public"),
     lib: {
-      entry: resolve(__dirname, 'src/main.ts'),
-      name: 'Connector',
-      formats: ['es', 'umd'],
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, "src/main.ts"),
+      name: "Connector",
+      formats: ["es", "umd"],
+      // the proper extensions will be added
       fileName: (format) => `connector-enabler.${format}.js`,
     },
   },
