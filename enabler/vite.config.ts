5741c903a2e2512c782b302d80f92a4b1e4e8d43
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-07-07 14:57:54 +0530

    Update vite.config.ts

--- enabler/vite.config.ts
+++ enabler/vite.config.ts
@@ -1,15 +1,8 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
 import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
-import { defineConfig } from 'vite';
-import path from 'path';
 
 export default defineConfig({
-    resolve: {
-    alias: {
-      '@prepayment': path.resolve(__dirname, '../prepayment/src'),
-    },
-  },
   plugins: [
     cssInjectedByJsPlugin({
       injectCodeFunction: function injectCodeCustomRunTimeFunction(
