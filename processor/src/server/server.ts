eda68aa73073546f253806b2998c0ac0a55a2d2c
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-07-11 13:20:10 +0530

    Update server.ts

--- processor/src/server/server.ts
+++ processor/src/server/server.ts
@@ -7,12 +7,9 @@ import { join } from 'path';
 import { config } from '../config/config';
 import { requestContextPlugin } from '../libs/fastify/context/context';
 import { errorHandler } from '../libs/fastify/error-handler';
-/**
- * Setup Fastify server instance
- * @returns
- */
+import { registerRoutes } from '../routes/mock-payment.route'; 
+
 export const setupFastify = async () => {
-  // Create fastify server instance
   const server = Fastify({
     logger: {
       level: config.loggerLevel,
@@ -22,24 +19,22 @@ export const setupFastify = async () => {
     requestIdHeader: 'x-request-id',
   });
 
-  // Setup error handler
   server.setErrorHandler(errorHandler);
 
-  // Enable CORS
   await server.register(cors, {
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Correlation-ID', 'X-Request-ID', 'X-Session-ID'],
     origin: '*',
   });
 
-  // Add content type parser for the content type application/x-www-form-urlencoded
   await server.register(fastifyFormBody);
 
-  // Register context plugin
   await server.register(requestContextPlugin);
 
   await server.register(autoLoad, {
     dir: join(__dirname, 'plugins'),
   });
+
+  await registerRoutes(server);
 
   return server;
 };
