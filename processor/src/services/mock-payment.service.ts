eca423e5bddbd007dc4f79d8eb529f9044323b90
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-07-16 19:21:08 +0530

    Update mock-payment.service.ts

--- processor/src/services/mock-payment.service.ts
+++ processor/src/services/mock-payment.service.ts
@@ -271,7 +271,7 @@ console.log('status-handler');
   }
 
  public async createPaymentt(request: CreatePaymentRequest): Promise<PaymentResponseSchemaDTO> {
-    return { success: true, id: 'mock-id' };
+    return { paymentReference: 'true'};
   }	
 
   /**
