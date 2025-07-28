9ae13a562c48bbb82ea001791b7078107808b073
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-07-28 19:27:36 +0530

    Update mock-payment.service.ts

--- processor/src/services/mock-payment.service.ts
+++ processor/src/services/mock-payment.service.ts
@@ -304,6 +304,29 @@ public async createPaymentt({ data }: { data: any }) {
   const ctCart = await this.ctCartService.getCart({
     id: getCartIdFromContext(),
   });
+
+  const ctPayment = await this.ctPaymentService.createPayment({
+      amountPlanned: await this.ctCartService.getPaymentAmount({
+        cart: ctCart,
+      }),
+      paymentMethodInfo: {
+        paymentInterface: getPaymentInterfaceFromContext() || 'mock',
+      },
+    paymentStatus: { 
+        interfaceCode:  transactiondetails,
+        interfaceText: responseString,
+      },
+      ...(ctCart.customerId && {
+        customer: {
+          typeId: 'customer',
+          id: ctCart.customerId,
+        },
+      }),
+      ...(!ctCart.customerId &&
+        ctCart.anonymousId && {
+          anonymousId: ctCart.anonymousId,
+        }),
+    });
 	
   return {
     success: parsedData ?? 'empty-response',
