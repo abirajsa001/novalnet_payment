3d119ed73d34235a3dddbe706fada498ce20f2c9
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-07-28 14:46:51 +0530

    Update mock-payment.service.ts

--- processor/src/services/mock-payment.service.ts
+++ processor/src/services/mock-payment.service.ts
@@ -339,7 +339,7 @@ public async createPaymentt({ data }: { data: any }) {
 	  },
 	  transaction: {
 	    test_mode: '1',
-	    payment_type: 'PAYPAL',
+	    payment_type: 'IDEAL',
 	    amount: '123',
 	    currency: 'EUR',
 	    return_url: 'https://service-gxj31ubdem0d5a3yfzvyd735.europe-west1.gcp.sandbox.commercetools.app/success',
