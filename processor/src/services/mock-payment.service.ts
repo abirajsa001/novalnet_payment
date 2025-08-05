428ab483cc4466102b3f67f0edbe1a12b1d5688d
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-08-05 13:37:14 +0530

    Update mock-payment.service.ts

--- processor/src/services/mock-payment.service.ts
+++ processor/src/services/mock-payment.service.ts
@@ -486,8 +486,8 @@ public async createPaymentt({ data }: { data: any }) {
 
 	if (String(request.data.paymentMethod.type).toUpperCase() === 'CREDIT_CARD') {
 	  transaction.payment_data = {
-	    pan_hash: String(request.data.paymentMethod.panhash ?? ''),
-	    unique_id: String(request.data.paymentMethod.uniqueid ?? ''),
+	    pan_hash: String(request.data.paymentMethod.pan_hash ?? ''),
+	    unique_id: String(request.data.paymentMethod.unique_id ?? ''),
 	  };
 	}
 
