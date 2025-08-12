74b7300e4103f31f2ed95c40f48c689998186271
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-08-12 15:25:35 +0530

    Update mock-payment.route.ts

--- processor/src/routes/mock-payment.route.ts
+++ processor/src/routes/mock-payment.route.ts
@@ -136,7 +136,7 @@ console.log('handle-novalnetResponse');
 	      custom: {
 	        fields: {
 	          status: query.status,
-	          source: 'redirect',
+	          source: getCartIdFromContext(),
 	        },
 	      },
 	    },
@@ -147,11 +147,10 @@ console.log('handle-novalnetResponse');
 	    },
 	  };
 
-  const result = await opts.paymentService.createPaymentt(createPaymentRequest);
+	 const result = await opts.paymentService.createPaymentt(createPaymentRequest);
 	
 
 	 const thirdPartyUrl = 'https://poc-novalnetpayments.frontend.site/en/thank-you/?orderId=c52dc5f2-f1ad-4e9c-9dc7-e60bf80d4a52';
-
 	 return reply.code(302).redirect(thirdPartyUrl);
 
       } catch (error) {
