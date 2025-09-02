dd301bbb65597e07efa52a2eb7b81c1cb06a7d98
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-09-02 11:11:15 +0530

    Update mock-payment.service.ts

--- processor/src/services/mock-payment.service.ts
+++ processor/src/services/mock-payment.service.ts
@@ -567,8 +567,6 @@ console.log('status-handler');
 	    inputval3: String(parsedCart.customerEmail ?? "Email not available"),
 	    input4: 'Payment-Method',
 	    inputval4: String(request.data.paymentMethod.type ?? "Payment-Method not available"),
-		input5: 'Get-Cart',
-		inputval5: String(Context.getCartIdFromContext() ?? 'empty-cart'),  
 	  }
 	};
 
@@ -648,7 +646,6 @@ console.log('status-handler');
     });
     return {
        paymentReference: updatedPayment.id,
-      // paymentReference: parsedResponse?.result?.redirect_url ?? 'null',
     };
   }
 	
