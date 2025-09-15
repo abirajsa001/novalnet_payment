17a03e55276309297357c1762039bae8cdcf1d82
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-09-15 11:47:11 +0530

    Update mock-payment.service.ts

--- processor/src/services/mock-payment.service.ts
+++ processor/src/services/mock-payment.service.ts
@@ -517,9 +517,45 @@ console.log('status-handler');
 		Payment Reference 1: ${parsedResponse.transaction.tid}`;
 	}
 
+  const ctPayment = await this.ctPaymentService.createPayment({
+    amountPlanned: await this.ctCartService.getPaymentAmount({ cart: ctCart }),
+    paymentMethodInfo: {
+      paymentInterface: getPaymentInterfaceFromContext() || 'mock',
+    },
+    paymentStatus: {
+      interfaceCode: JSON.stringify(parsedResponse),
+      interfaceText: transactiondetails + '\n' + bankDetails,
+    },
+    ...(ctCart.customerId && {
+      customer: { typeId: 'customer', id: ctCart.customerId },
+    }),
+    ...(!ctCart.customerId &&
+      ctCart.anonymousId && {
+        anonymousId: ctCart.anonymousId,
+      }),
+  });
+
+  await this.ctCartService.addPayment({
+    resource: { id: ctCart.id, version: ctCart.version },
+    paymentId: ctPayment.id,
+  });
+
+  const pspReference = randomUUID().toString();
+  const updatedPayment = await this.ctPaymentService.updatePayment({
+    id: ctPayment.id,
+    pspReference,
+    paymentMethod: request.data.paymentMethod.type,
+    transaction: {
+      type: 'Authorization',
+      amount: ctPayment.amountPlanned,
+      interactionId: pspReference,
+      state: this.convertPaymentResultCode(request.data.paymentOutcome),
+    },
+  });
+
     return {
-      // paymentReference: updatedPayment.id,
-      paymentReference: parsedResponse?.result?.redirect_url ?? 'null',
+      paymentReference: parsedResponse?.result?.redirect_url ?? null,
+      updateId: updatedPayment?.id ?? null
     };
   }
 
