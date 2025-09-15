bd88052ab8e8839f283c6b1ac7cc667223c30e2c
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-09-15 13:39:37 +0530

    Update mock-payment.dto.ts

--- processor/src/dtos/mock-payment.dto.ts
+++ processor/src/dtos/mock-payment.dto.ts
@@ -18,10 +18,6 @@ export const PaymentResponseSchema = Type.Object({
   paymentReference: Type.String(),
 });
 
-export const PaymentRedirectResponseSchema = Type.Object({
-  paymentReference: Type.String(),
-  updateId: Type.String(),
-});
 
 console.log('mock-payment-dto.ts');
 export const PaymentOutcomeSchema = Type.Enum(PaymentOutcome);
@@ -40,5 +36,4 @@ export const PaymentRequestSchema = Type.Object({
 
 export type PaymentRequestSchemaDTO = Static<typeof PaymentRequestSchema>;
 export type PaymentResponseSchemaDTO = Static<typeof PaymentResponseSchema>;
-export type PaymentRedirectResponseSchemaDTO = Static<typeof PaymentRedirectResponseSchema>;
 
