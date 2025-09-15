54b9f9d61f0d93b9428a464a0e9a02c587e9e073
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-09-15 12:01:43 +0530

    Update ideal.ts

--- enabler/src/components/payment-methods/ideal/ideal.ts
+++ enabler/src/components/payment-methods/ideal/ideal.ts
@@ -94,7 +94,6 @@ export class Ideal extends BaseComponent {
       console.log(response);
       const data = await response.json();
       console.log(data);
-     console.log(data.updateId);
 
      if(data.paymentReference) {
        location.href = data.paymentReference;
