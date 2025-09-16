35048f3f0cf9ae0f2f5c3a529cb758d858d195fe
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-09-16 13:07:03 +0530

    Update ideal.ts

--- enabler/src/components/payment-methods/ideal/ideal.ts
+++ enabler/src/components/payment-methods/ideal/ideal.ts
@@ -76,7 +76,7 @@ export class Ideal extends BaseComponent {
       const requestData: PaymentRequestSchemaDTO = {
         paymentMethod: {
           type: this.paymentMethod,
-          returnUrl: "testurl",
+          returnUrl: window.location.href,
         },
         paymentOutcome: PaymentOutcome.AUTHORIZED,
       };
