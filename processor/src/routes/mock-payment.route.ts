fb15257a3c082f6008aa4add544e8f7b8c406ba1
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-09-17 14:51:29 +0530

    Update mock-payment.route.ts

--- processor/src/routes/mock-payment.route.ts
+++ processor/src/routes/mock-payment.route.ts
@@ -10,6 +10,14 @@ import {
   PaymentResponseSchema,
   PaymentResponseSchemaDTO,
 } from '../dtos/mock-payment.dto';
+
+import {
+  ComponentOptions,
+  PaymentComponent,
+  PaymentComponentBuilder,
+  PaymentMethod
+} from '../../payment-enabler/payment-enabler';
+
 import { MockPaymentService } from '../services/mock-payment.service';
 import { log } from '../libs/logger';
 type PaymentRoutesOptions = {
