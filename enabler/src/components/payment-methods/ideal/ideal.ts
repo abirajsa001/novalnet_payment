4511f9c835e83e64b48d95c7000db5b71f889611
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-09-30 19:45:20 +0530

    Update ideal.ts

--- enabler/src/components/payment-methods/ideal/ideal.ts
+++ enabler/src/components/payment-methods/ideal/ideal.ts
@@ -12,7 +12,7 @@ import {
   PaymentRequestSchemaDTO,
 } from "../../../dtos/mock-payment.dto";
 import { BaseOptions } from "../../../payment-enabler/payment-enabler-mock";
-// import { getConfig } from "../../../config/config";
+import { checkoutFlow } from '@commercetools/checkout-browser-sdk';
 
 export class IdealBuilder implements PaymentComponentBuilder {
   public componentHasSubmit = true;
