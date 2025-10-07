b06b81bce59150f92f5dea0a9bda02b66bb5706d
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-10-07 17:59:03 +0530

    demo

--- processor/src/routes/mock-payment.route.ts
+++ processor/src/routes/mock-payment.route.ts
@@ -103,9 +103,10 @@ export const paymentRoutes = async (
         const resp = await opts.paymentService.createPayments({
           data: request.body,
         });
+        const returnURL = resp.txnSecret;
         log.info("Payment service response:", JSON.stringify(resp, null, 2));
         // return reply.status(200).send(resp);
-        return reply.code(302).redirect(resp.txnSecret);
+        return reply.code(302).redirect(returnURL);
       } catch (error) {
         log.error("Payment route error:", error);
         const errorMessage = error instanceof Error ? error.message : 'Unknown error';
