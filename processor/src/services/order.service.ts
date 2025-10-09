13864c648d17a94c6fc048f3313b382c275989b6
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-10-09 16:53:06 +0530

    aync removed 2

--- processor/src/services/order.service.ts
+++ processor/src/services/order.service.ts
@@ -1,8 +1,8 @@
 import { getApiRoot } from '../utils/ct-client';
 import { Order } from '@commercetools/platform-sdk';
 
-export function getOrderByOrderNumber(orderNumber: string): Promise<Order | null> {
-  try {
+// export function getOrderByOrderNumber(orderNumber: string): Promise<Order | null> {
+//   try {
     // const apiRoot = getApiRoot();
 
     // const response = await apiRoot
@@ -12,14 +12,14 @@ export function getOrderByOrderNumber(orderNumber: string): Promise<Order | null
     //   .execute();
 
     // return response.body;
-  } catch (error: any) {
+  // } catch (error: any) {
     // if (error.statusCode === 404) return null;
     // console.error('Error fetching order:', error);
     // throw error;
-  }
-}
+//   }
+// }
 
-export async function getOrderIdFromOrderNumber(orderNumber: string): Promise<string | null> {
+export function getOrderIdFromOrderNumber(orderNumber: string): Promise<string | null> {
   // const order = await getOrderByOrderNumber(orderNumber);
   // const order = await getOrderByOrderNumber(orderNumber);
   // return order ? order.id : null;
