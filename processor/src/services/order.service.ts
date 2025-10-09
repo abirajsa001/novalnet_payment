58633516381ec71a6e139b5bc968d5297ec1ed5b
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-10-09 16:12:47 +0530

    order service code commented

--- processor/src/services/order.service.ts
+++ processor/src/services/order.service.ts
@@ -1,25 +1,25 @@
 import { getApiRoot } from '../utils/ct-client';
 import { Order } from '@commercetools/platform-sdk';
 
-export async function getOrderByOrderNumber(orderNumber: string): Promise<Order | null> {
-  try {
-    const apiRoot = getApiRoot();
+// export async function getOrderByOrderNumber(orderNumber: string): Promise<Order | null> {
+  // try {
+  //   const apiRoot = getApiRoot();
 
-    const response = await apiRoot
-      .orders()
-      .withOrderNumber({ orderNumber })
-      .get()
-      .execute();
+  //   const response = await apiRoot
+  //     .orders()
+  //     .withOrderNumber({ orderNumber })
+  //     .get()
+  //     .execute();
 
-    return response.body;
-  } catch (error: any) {
-    // if (error.statusCode === 404) return null;
-    // console.error('Error fetching order:', error);
-    // throw error;
-  }
-}
+  //   return response.body;
+  // } catch (error: any) {
+  //   // if (error.statusCode === 404) return null;
+  //   // console.error('Error fetching order:', error);
+  //   // throw error;
+  // }
+// }
 
-export async function getOrderIdFromOrderNumber(orderNumber: string): Promise<string | null> {
-  const order = await getOrderByOrderNumber(orderNumber);
-  return order ? order.id : null;
-}
+// export async function getOrderIdFromOrderNumber(orderNumber: string): Promise<string | null> {
+  // const order = await getOrderByOrderNumber(orderNumber);
+  // return order ? order.id : null;
+// }
