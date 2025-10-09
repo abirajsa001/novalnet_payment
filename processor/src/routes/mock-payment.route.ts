f097a08f1d29c1711d59ed2ca83a162e14e0aa98
abirajsa001 <abiraj_s@novalnetsolutions.com>
2025-10-09 15:45:51 +0530

    order service file changed to txt

--- processor/src/routes/mock-payment.route.ts
+++ processor/src/routes/mock-payment.route.ts
@@ -1,5 +1,5 @@
 import { SessionHeaderAuthenticationHook } from "@commercetools/connect-payments-sdk";
-import { getOrderIdFromOrderNumber } from '../services/order.service';
+// import { getOrderIdFromOrderNumber } from '../services/order.service';
 
 import {
   FastifyInstance,
@@ -176,12 +176,12 @@ export const paymentRoutes = async (
       
       if (generatedChecksum === query.checksum) {
         try {
-          const orderId = await getOrderIdFromOrderNumber(orderNumber);
-          if (!orderId) return reply.code(404).send('Order not found');
+          // const orderId = await getOrderIdFromOrderNumber(orderNumber);
+          // if (!orderId) return reply.code(404).send('Order not found');
 
-          const thirdPartyUrl = 'https://poc-novalnetpayments.frontend.site/en/thank-you/?orderId=' + orderId;
+          // const thirdPartyUrl = 'https://poc-novalnetpayments.frontend.site/en/thank-you/?orderId=' + orderId;
 
-          // const thirdPartyUrl = 'https://poc-novalnetpayments.frontend.site/en/thank-you/';
+          const thirdPartyUrl = 'https://poc-novalnetpayments.frontend.site/en/thank-you/';
           return reply.code(302).redirect(thirdPartyUrl);
         } catch (error) {
           log.error("Error processing payment:", error);
